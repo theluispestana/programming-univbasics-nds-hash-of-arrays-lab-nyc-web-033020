4b0277a0d8a84aa8f2ff64cd8c009343841ceb64
Luis Pestana <lpestana@protonmail.com>
2020-02-28 15:49:42 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,7 +8,7 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
-  show = {
-    name
+  hoa = {
+    show => name
   }
 end
